92aeb91c29f6b77d5e1647aca4018bda41944254
aniqoff-DEV <andrey.emailforwork.dev@gmail.com>
2024-01-11 03:16:24 +0300

    added sorting by type via button

--- SASP.Client/Pages/IssuesPage.xaml.cs
+++ SASP.Client/Pages/IssuesPage.xaml.cs
@@ -6,10 +6,15 @@ namespace SASP.Client.Pages;
 public partial class IssuesPage : ContentPage
 {
     private readonly IRestDataService<Issue, IssueDto> _issueDataService;
-    private string SelectedText;
+
+    private string selectedButtonText;
+
+    private Button lastButton;
+
     public IssuesPage(IRestDataService<Issue, IssueDto> issueDataService)
 	{
 		InitializeComponent();
+
         _issueDataService = issueDataService;
 	}
 
@@ -20,19 +25,42 @@ public partial class IssuesPage : ContentPage
         IssuesDataView.ItemsSource = await _issueDataService.GetAllAsync();
     }
 
-    private void Button_Clicked(object sender, EventArgs e)
+    private async void Button_Clicked(object sender, EventArgs e)
     {
-        SelectedText = ((Button)sender).Text;
+        if(lastButton != null) 
+        {
+            lastButton.BackgroundColor = Color.FromArgb("#512BD4");
+        }
+        
+        lastButton = (Button)sender;
+        selectedButtonText = ((Button)sender).Text;
+
+        ((Button)sender).BackgroundColor = Colors.Blue;
+
+        IssuesSource();
     }
 
-    private async void SearchEntry_TextChanged(object sender, TextChangedEventArgs e)
+    private void SearchEntry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        IssuesSource();
+    }
+
+    private async void IssuesSource()
     {
         var currentIssues = await _issueDataService.GetAllAsync();
 
-        currentIssues = currentIssues.Where(i =>
-            i.Title.ToLower().Contains(SearchEntry.Text.ToLower()) ||
-            i.Catalog.ToLower().Contains(SearchEntry.Text.ToLower()))
-            .ToList();
+        if(selectedButtonText != null)
+        {
+            currentIssues = currentIssues.Where(i => i.TypeIssue.Contains(selectedButtonText)).ToList();
+        }
+
+        if(SearchEntry.Text != null)
+        {
+            currentIssues = currentIssues.Where(i =>
+                i.Title.ToLower().Contains(SearchEntry.Text.ToLower()) ||
+                i.Catalog.ToLower().Contains(SearchEntry.Text.ToLower()))
+                .ToList();
+        }
 
         IssuesDataView.ItemsSource = currentIssues.OrderBy(i => i.Title).ToList();
     }
